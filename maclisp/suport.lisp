b732229e4538fc9919e750e4847dbb557af10970
Owain Kenway <o.kenway@ucl.ac.uk>
2021-02-19 12:06:25 +0000

    Rounding and floor functions.

--- maclisp/suport.lisp
+++ maclisp/suport.lisp
@@ -3,3 +3,12 @@
 (defun ceiling (x) (if (greaterp x (fix x)) (+ 1 (fix x)) (fix x)))
 
 (defun evenp (x) (not (oddp x)))
+
+(defun round (x) 
+        (if (greaterp x 0)
+                (fix (+$ 0.5 (float x)))
+                (* -1 (fix (+$ 0.5 (*$ -1.0 (float x)))))
+        )
+)
+
+(defun floor (x) (fix x))
