d230242a218320ec7b719fa5169d6cf5bb327540
Owain Kenway <o.kenway@ucl.ac.uk>
2022-03-01 13:37:36 +0000

    Code to test if a number is prime

---
+++ cobol/3/isprime.cbl
@@ -0,0 +1,30 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. isprime.
+       AUTHOR. Owain Kenway. 
+
+       DATA DIVISION.
+          WORKING-STORAGE SECTION.
+             01 temp USAGE IS BINARY-LONG UNSIGNED VALUE 0.
+             01 x USAGE IS BINARY-LONG UNSIGNED VALUE 0.
+             01 temp2 USAGE IS FLOAT-LONG.
+             01 m USAGE IS BINARY-LONG UNSIGNED.
+
+          LINKAGE SECTION.
+             01 n USAGE IS BINARY-LONG UNSIGNED.
+             01 d USAGE IS BINARY-LONG UNSIGNED.
+
+       PROCEDURE DIVISION USING n, d.
+          MOVE 0 TO temp.
+
+          MOVE FUNCTION SQRT(n) TO temp2.
+          ADD 1 TO temp2.
+          MOVE FUNCTION INTEGER(temp2) TO m.
+
+          CALL 'largestfactor' USING n, temp, m.
+
+          IF temp IS EQUAL TO 0 THEN
+             MOVE n TO d
+          ELSE
+             MOVE 0 TO d
+          END-IF.
+       EXIT PROGRAM.
---
+++ cobol/3/testisprime.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. testisprime.
+       AUTHOR. Owain Kenway. 
+
+       DATA DIVISION.
+          WORKING-STORAGE SECTION.
+             01 n USAGE IS BINARY-LONG UNSIGNED VALUE 20.
+             01 soln USAGE IS BINARY-LONG UNSIGNED VALUE 0.
+             01 x USAGE IS BINARY-LONG UNSIGNED.
+
+       PROCEDURE DIVISION.
+          PERFORM VARYING x FROM 2 BY 1
+             UNTIL x IS GREATER THAN OR EQUAL TO n
+             CALL 'isprime' USING x, soln
+
+             DISPLAY x ": " soln
+          END-PERFORM.
+       STOP RUN.
