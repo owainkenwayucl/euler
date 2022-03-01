f5d0ac4011a08129ab2d5caa128789ccd6d1c129
Owain Kenway <o.kenway@ucl.ac.uk>
2022-03-01 13:04:27 +0000

    Subroutine for finding largest factor.

---
+++ cobol/3/largestfactor.cbl
@@ -0,0 +1,31 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. largestfactor.
+       AUTHOR. Owain Kenway. 
+
+       DATA DIVISION.
+          WORKING-STORAGE SECTION.
+             01 x USAGE IS BINARY-LONG UNSIGNED.
+             01 temp USAGE IS BINARY-LONG UNSIGNED.
+             01 temp2 USAGE IS FLOAT-LONG.
+
+          LINKAGE SECTION.
+             01 n USAGE IS BINARY-LONG UNSIGNED.
+             01 f USAGE IS BINARY-LONG UNSIGNED.
+             01 m USAGE IS BINARY-LONG UNSIGNED.
+
+      * Works out the largest factor of n that's less than or equal to m,
+      * stores in f. 
+       PROCEDURE DIVISION USING n, f, m.
+          MOVE 0 TO f.
+          MOVE m TO temp2
+          ADD 1 TO temp2
+          PERFORM VARYING x FROM 2 BY 1
+             UNTIL x IS GREATER THAN OR EQUAL TO temp2
+             CALL 'divides' USING n, x, temp
+             IF temp IS EQUAL TO 1 THEN
+                IF x IS GREATER THAN f THEN
+                   MOVE x TO f
+                END-IF
+             END-IF
+          END-PERFORM.
+       EXIT PROGRAM.
---
+++ cobol/3/testlargestfactor.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. testlargestfactor.
+       AUTHOR. Owain Kenway. 
+
+       DATA DIVISION.
+          WORKING-STORAGE SECTION.
+             01 n USAGE IS BINARY-LONG UNSIGNED VALUE 20.
+             01 soln USAGE IS BINARY-LONG UNSIGNED VALUE 0.
+             01 x USAGE IS BINARY-LONG UNSIGNED.
+
+       PROCEDURE DIVISION.
+          PERFORM VARYING x FROM 2 BY 1
+             UNTIL x IS GREATER THAN OR EQUAL TO n
+             CALL 'largestfactor' USING n, soln, x 
+
+             DISPLAY x ": " soln
+          END-PERFORM.
+       STOP RUN.
